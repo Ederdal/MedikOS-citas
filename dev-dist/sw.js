7f665ef93f6137fccde15ff496f9f1f118d59e5f
Ederdal <degarcruz2004@gmail.com>
2025-12-15 08:37:39 -0600

    correciono de graficas
    upd: se modifico la grafica de Notas medicas

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vcqkcu9qqsk"
+    "revision": "0.ieeh60n08js"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
